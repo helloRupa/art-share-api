4d57a6edd0092c5bea2b2da7eccd23487b88cd30
Rupa Dhillon <rupa211984@gmail.com>
2019-04-25 16:49:24 -0700

    Complete Comments Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :artworks, only: [:update, :create, :destroy, :show]
   resources :artwork_shares, only: [:create, :destroy]
+  resources :comments, only: [:create, :destroy]
 
   resources :users, only: [:index, :update, :create, :destroy, :show] do
     resources :artworks, only: [:index]
+    resources :comments, only: [:index]
+  end
+
+  resources :artworks, only: [:update, :create, :destroy, :show] do
+    resources :comments, only: [:index]
   end
 end
